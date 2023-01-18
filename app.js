d4c9acb25e479ab1f07b18fbb5a08f0efeeb7422
amadijeffrey <amadichibuike72@gmail.com>
2023-01-18 22:30:42 +0100

    add vercel config file

--- app.js
+++ app.js
@@ -19,16 +19,13 @@ app.use(cors({
     optionSuccessStatus:200
 }))
 
-app.use(express.static(path.join(__dirname,'build')))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.use('/api/products', productRoutes)
 app.use(authRoutes)
 
-app.get('*', (res) => {
-    res.sendFile(path.join(__dirname,'build', 'index.html'))
-})
+
 
 app.listen(port, () => {
     console.log('server is running')
